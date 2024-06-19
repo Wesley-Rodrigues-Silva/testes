ec9936062871beaa4b38ce36e1d5cb66666a6c93
Wesley Rodrigues Silva <161177795+Wesley-Rodrigues-Silva@users.noreply.github.com>
2024-06-19 17:57:36 -0300

    att

---
+++ foreach/js/script.js
@@ -0,0 +1,12 @@
+const lista = [
+    {nome: 'Wesley', idade: 26, profissao: 'programador'},
+    {nome: 'João', idade: 16, profissao: 'médico'},
+    {nome: 'Maria', idade: 31, profissao: 'enfermeira'},
+    {nome: 'Isabela', idade: 21, profissao: 'professora'},
+    {nome: 'Gabriel', idade: 33, profissao: 'cirurgião'},
+
+]
+
+lista.forEach((itens)=> {
+    console.log(itens)
+})
